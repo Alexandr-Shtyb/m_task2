d7f40ea24358a3e09b970b276f5c3fd0f388e997
Alexandr-Shtyb <styb701@mail.ru>
2024-03-21 20:16:11 +0300

    feat: create sign up with firebase

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,34 @@
+import { db, auth, googleProvider } from "../firebase/firebase";
+import { collection, addDoc } from "firebase/firestore";
+import { signInWithPopup } from "firebase/auth";
 import AppContainer from "../components/layout/AppContainer";
+import { Flex, Button } from "@mantine/core";
+import { useTranslation } from "react-i18next";
 
 const Auth = () => {
-  return <AppContainer>HOME</AppContainer>;
+  const { t } = useTranslation();
+
+  const login = async () => {
+    try {
+      const { user } = await signInWithPopup(auth, googleProvider);
+      await addDoc(collection(db, "users"), {
+        name: user.displayName,
+        email: user.email,
+        role: "user",
+      });
+      console.log(user);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <AppContainer>
+      <Flex pt={100} justify="center" align="center">
+        <Button onClick={login}>{t("authPage.textButtonLogin")}</Button>
+      </Flex>
+    </AppContainer>
+  );
 };
 
 export default Auth;
